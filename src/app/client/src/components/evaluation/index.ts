09de6466d91e8e2a6295c1fb2b0c6dfab6b1cece
Nikhil Kadapala <nikhil.kadapala@unh.edu>
2025-06-18 15:26:15 -0500

    Remove evaluation components and utility functions related to claim extraction using old naming conventions.

--- src/app/client/src/components/evaluation/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as SystemMessageCard } from './SystemMessageCard';
-export { default as ModelSelectionCard } from './ModelSelectionCard';
-export { default as PromptSelectionCard } from './PromptSelectionCard';
-export { default as EvaluationMethodsCard } from './EvaluationMethodsCard';
-export { default as EvaluationMetricsCard } from './EvaluationMetricsCard';
-export { default as FileUploadCard } from './FileUploadCard';
-export { default as EvaluationProgressCard } from './EvaluationProgressCard'; 
