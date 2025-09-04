41bb67dd853972c3dc97287df241cb198892e992
Nikhil Kadapala <nikhil.kadapala@unh.edu>
2025-09-04 14:50:57 -0500

    Refactor get_model_response function to utilize LLMRouter for model initialization, streamlining response generation for various model types. Update imports for NormalizedClaim and Feedback schemas to reflect new structure.

--- api/utils/get_model_response.py
+++ api/utils/get_model_response.py
@@ -1,10 +1,13 @@
 from typing import Union, Generator
-from .schema import NormalizedClaim, Feedback
+from schemas.claims import NormalizedClaim
+from schemas.feedback import Feedback
 from .openai import OpenAIModel
 from .xai import get_grok_structured_response, get_grok_streaming_response
 from .togetherAI import get_llama_streaming_response, get_llama_structured_response
 from .gemini import get_gemini_structured_response, get_gemini_streaming_response
 from .anthropic import get_claude_structured_response, get_claude_streaming_response
+from .LLMRouter import LLMRouter
+from .llms import OPENAI_MODELS, xAI_MODELS, TOGETHER_MODELS, ANTHROPIC_MODELS, GEMINI_MODELS
 
 def get_model_response(model:str, user_prompt:str, sys_prompt:str, gen_type:str)->Union[NormalizedClaim, Feedback, Generator[str, None, None]]:
     """ This function takes a user prompt and a system prompt, and returns the generated response of the model using OpenAI's API.
@@ -28,28 +31,10 @@ def get_model_response(model:str, user_prompt:str, sys_prompt:str, gen_type:str)
     elif gen_type == "feedback":
         response_type = Feedback
     
-    if model in ["gpt-4o-2024-11-20", "gpt-4.1-2025-04-14", "gpt-4.1-nano-2025-04-14"]:
-        MODEL = OpenAIModel(model=model)
-        if gen_type == "chat":
-            generated_claim = MODEL.generate_streaming_response(sys_prompt, user_prompt)
-        elif gen_type in ["init", "refine", "feedback"]:
-            generated_claim = MODEL.generate_structured_response(sys_prompt, user_prompt, response_type)
-    elif model in ["grok-3","grok-3-mini", "grok-3-mini-fast"]:
-        generated_claim = get_grok_streaming_response(model, sys_prompt, user_prompt) if gen_type == "chat" else get_grok_structured_response(model, sys_prompt, user_prompt, response_type)
-             
-    elif model in ["meta-llama/Llama-3.3-70B-Instruct-Turbo-Free", "deepseek-ai/DeepSeek-R1-Distill-Llama-70B-free"]:
-        if gen_type == "chat":
-            generated_claim = get_llama_streaming_response(model, sys_prompt, user_prompt)
-        else:
-            generated_claim = get_llama_structured_response(model, sys_prompt, user_prompt, response_type)
-
-    elif model in ["gemini-2.5-pro-preview-05-06", "gemini-2.5-flash-preview-04-17"]:
-        generated_claim = get_gemini_streaming_response(model, sys_prompt, user_prompt) if gen_type == "chat" else get_gemini_structured_response(model, sys_prompt, user_prompt, response_type)
-        
-    elif model in ["claude-sonnet-4-20250514", "claude-opus-4-20250514"]:
-        generated_claim = get_claude_streaming_response(model, sys_prompt, user_prompt) if gen_type == "chat" else get_claude_structured_response(model, sys_prompt, user_prompt, response_type)
-    
-    else:
-        raise ValueError(f"Unsupported model: {model}")
+    MODEL = LLMRouter(model=model).get_api_client()
+    if gen_type == "chat":
+        generated_claim = MODEL.generate_streaming_response(sys_prompt, user_prompt)
+    elif gen_type in ["init", "refine", "feedback"]:
+        generated_claim = MODEL.generate_structured_response(sys_prompt, user_prompt, response_type)
             
     return generated_claim
