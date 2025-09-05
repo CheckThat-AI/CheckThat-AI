07d337f7f5eb8a7271b6758b2cb9daf36d42c378
Nikhil Kadapala <nikhil.kadapala@unh.edu>
2025-09-04 19:44:42 -0500

    Refactor API router to include chat module while removing unused route inclusions.

--- api/routes/__init__.py
+++ api/routes/__init__.py
@@ -7,6 +7,3 @@ api_router = APIRouter()
 # Include all route modules
 api_router.include_router(health.router)
 api_router.include_router(chat.router)
-api_router.include_router(eval.router)
-api_router.include_router(drive.router)
-api_router.include_router(auth.router) 
